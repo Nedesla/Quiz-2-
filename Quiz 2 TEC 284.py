b3e415a4d9df837508f7c391d7e1f9981619e624
Nedesla <157523223+Nedesla@users.noreply.github.com>
2025-04-10 15:33:49 -0500

    Commit 3
    Final Commit

--- Quiz 2 TEC 284.py
+++ Quiz 2 TEC 284.py
@@ -1,15 +1,21 @@
+##Imported libraries
 import time
 from gpiozero import LED, Button
 
+##Push buttons with pins
 redPushbutton = Button (17)
 greenPushbutton = Button (27)
 bluePushbutton = Button (22)
 
+#LED's with pins
 redLed = LED (26)
 greenLed = LED (19)
 blueLed = LED (13)
+
+#While loop 
 while True:
     
+    ##If Statements
     if redPushbutton.is_pressed:
     
         redLed.on()
