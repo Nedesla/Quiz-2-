77de00ec5e7159742c995f0ad4673494ea9d71fc
Nedesla <157523223+Nedesla@users.noreply.github.com>
2025-04-10 15:31:53 -0500

    Commit 2
    Functional Leds

--- Quiz 2 TEC 284.py
+++ Quiz 2 TEC 284.py
@@ -1,11 +1,40 @@
 import time
 from gpiozero import LED, Button
 
-redPushbutton = 17
-greenPushbutton = 27
-bluePushbutton = 22
+redPushbutton = Button (17)
+greenPushbutton = Button (27)
+bluePushbutton = Button (22)
 
 redLed = LED (26)
 greenLed = LED (19)
 blueLed = LED (13)
+while True:
+    
+    if redPushbutton.is_pressed:
+    
+        redLed.on()
+    
+        time.sleep(1)
+    
+    else:
+        redLed.off()
+        
+    if greenPushbutton.is_pressed:
+    
+        greenLed.on()
+    
+        time.sleep(1)
+    
+    else:
+        greenLed.off()
+        
+    if bluePushbutton.is_pressed:
+    
+        blueLed.on()
+    
+        time.sleep(1)
+    
+    else:
+        blueLed.off()
 
+    
